a65153f5aaf2c901351e8a5203f9eb4cd38d3806
AbhishekJ <abhishek.jamodkar@okta.com>
2024-03-11 23:23:26 -0400

    updated auth0 product pages

--- product/auth0/js/ui.js
+++ product/auth0/js/ui.js
@@ -1,57 +1,3 @@
-// URL mapping, from hash to a function that responds to that URL action
-const router = {
-  "/": () => showContent("content-home"),
-  "/profile": () =>
-    requireAuth(() => showContent("content-profile"), "/profile"),
-  "/login": () => login()
-};
-
-//Declare helper functions
-
-/**
- * Iterates over the elements matching 'selector' and passes them
- * to 'fn'
- * @param {*} selector The CSS selector to find
- * @param {*} fn The function to execute for every element
- */
-const eachElement = (selector, fn) => {
-  for (let e of document.querySelectorAll(selector)) {
-    fn(e);
-  }
-};
-
-/**
- * Tries to display a content panel that is referenced
- * by the specified route URL. These are matched using the
- * router, defined above.
- * @param {*} url The route URL
- */
-const showContentFromUrl = (url) => {
-  if (router[url]) {
-    router[url]();
-    return true;
-  }
-
-  return false;
-};
-
-/**
- * Returns true if `element` is a hyperlink that can be considered a link to another SPA route
- * @param {*} element The element to check
- */
-const isRouteLink = (element) =>
-  element.tagName === "A" && element.classList.contains("route-link");
-
-/**
- * Displays a content panel specified by the given element id.
- * All the panels that participate in this flow should have the 'page' class applied,
- * so that it can be correctly hidden before the requested content is shown.
- * @param {*} id The id of the content to show
- */
-const showContent = (id) => {
-  eachElement(".page", (p) => p.classList.add("hidden"));
-  document.getElementById(id).classList.remove("hidden");
-};
 
 /**
  * Updates the user interface
@@ -59,8 +5,21 @@ const showContent = (id) => {
 const updateUI = async () => {
   try {
     const isAuthenticated = await auth0Client.isAuthenticated();
-
+    
     if (isAuthenticated) {
+
+      var elementsToShow = document.getElementsByClassName('auth-show');
+      for (var i = 0; i < elementsToShow.length; i++) {
+        // Remove the class
+        elementsToShow[i].classList.remove('visually-hidden');
+    }
+    var elementsToHide = document.getElementsByClassName('auth-hide');
+      for (var i = 0; i < elementsToHide.length; i++) {
+        // Remove the class
+        elementsToHide[i].classList.add('visually-hidden');
+    }
+
+
       const user = await auth0Client.getUser();
       const claims = await auth0Client.getIdTokenClaims();
       const id_token = claims.__raw;
